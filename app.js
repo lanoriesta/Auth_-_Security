6e6b38227f1b0195a8ad7bb9fa42c6dba1189c74
Lawrence Noriesta <lanoriesta@gmai.com>
2023-04-05 00:25:36 +0800

    bcrypt hash

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const md5 = require('md5');
+// const md5 = require('md5');
+const bcrypt = require('bcrypt');
 
 const app = express();
 const route = 3000;
+const saltRound = 10;
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('public'));
@@ -46,28 +48,39 @@ app.get('/register', (req, res) => {
 
 
 app.post('/register', (req, res) => {
-    
-    const newUSer = new User({
-        email: req.body.username,
-        password: md5(req.body.password)
-    });
 
-    newUSer.save().then(() => {
-        res.render('secrets');
-    }).catch(err => console.log(err));
+    bcrypt.hash(req.body.password, saltRound).then( hash => {
+
+        const newUSer = new User({
+            email: req.body.username,
+            password: hash
+        });
+    
+        newUSer.save().then(() => {
+            res.render('secrets');
+        }).catch(err => console.log(err));
+
+    }).catch( err => console.log(err))
+    
+    
 
 });
 
 app.post('/login', (req, res) => {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
 
     User.findOne({email: username}).then( foundUser => {
 
         if (foundUser) {
-            if (foundUser.password === password) {
-                res.render('secrets');
-            }
+            
+            bcrypt.compare(password, foundUser.password).then( result => {
+                if (result === true) {
+                    res.render('secrets');
+                }
+            }).catch( err => console.log(err))
+                
+            
         }
 
     }).catch(err => console.log(err));
